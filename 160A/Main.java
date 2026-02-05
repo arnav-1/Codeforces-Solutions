659175e50b07bbd212c90bfac25cbc2c12509c5c
Arnav Jaiswal <arnavjaiswal05@gmail.com>
2026-02-05 22:08:34 +0530

    Fixed 160A folder structure

--- 160A/Main.java
+++ 160A/Main.java
@@ -1,1 +1,27 @@
 import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] value = new int[n];
+        int sum=0;
+        for (int i=0;i<n;i++){
+            value[i]=sc.nextInt();
+            sum=sum+value[i];
+        }
+        Arrays.sort(value);
+        int larger=(sum/2)+1;
+        int count=0;
+        int mysum=0;
+        for(int i=n-1;i>=0;i--){
+            mysum=mysum+value[i];
+            count++;
+                if(mysum>=larger){
+                    System.out.println(count);
+                    break;
+                }
+
+        }
+    }
+
+}
