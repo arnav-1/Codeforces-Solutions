bef9b588814bc73d57540e1819df95f13465072d
Arnav Jaiswal <arnavjaiswal05@gmail.com>
2026-02-21 00:14:38 +0530

    Solved 116A Tram

---
+++ 116A/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int currentPassengers = 0;
+        int maxCapacity = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int exit = sc.nextInt();
+            int enter = sc.nextInt();
+            currentPassengers = currentPassengers - exit + enter;
+            if (currentPassengers > maxCapacity) {
+                maxCapacity = currentPassengers;
+            }
+        }
+        
+        System.out.println(maxCapacity);
+    }
+}
