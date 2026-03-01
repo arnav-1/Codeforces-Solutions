ed11d7528d9f05eb9994959b9aac65c1dcba7ce0
Arnav Jaiswal <arnavjaiswal05@gmail.com>
2026-03-01 21:18:57 +0530

    Solved 486A Calculating Function

---
+++ 486A/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        long n = sc.nextLong();
+        long sum;
+        
+        if (n % 2 == 0) {
+            sum = n / 2;
+        } else {
+            sum = -(n + 1) / 2;
+        }
+        
+        System.out.println(sum);
+    }
+}
