619f8644ef73d27bc31b4d3000bdb4de6e1c6753
Arnav Jaiswal <arnavjaiswal05@gmail.com>
2026-03-02 23:15:31 +0530

    Solved 228A Is your horseshoe on the other hoof

--- 486A/Main.java
+++ 486A/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        long n = sc.nextLong();
+            long n = sc.nextLong();
         long sum;
         
         if (n % 2 == 0) {
